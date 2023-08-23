54e5a21f3e6e18777c22bc4c0093cdc1e3072aa7
sann-htet <sannhtet899@gmail.com>
2023-08-23 20:50:18 +0630

    created a deeper CNN model

--- src/model.py
+++ src/model.py
@@ -30,3 +30,40 @@ def create_model():
     model.add(Dense(10, activation='softmax'))
     
     return model
+
+def create_deeper_model():
+    model = Sequential()
+
+    # Add the CustomPreprocessNormalizationLayer as the first layer
+    model.add(CustomPreprocessNormalizationLayer())
+
+    # Convolutional layer block 1
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1), padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.5))
+
+    # Convolutional layer block 2
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.5))
+
+    # Convolutional layer block 3
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(0.5))
+
+    # Flatten the output for Dense layers
+    model.add(Flatten())
+
+    # Dense layers
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+
+    # Output layer
+    model.add(Dense(10, activation='softmax'))
+
+    return model
